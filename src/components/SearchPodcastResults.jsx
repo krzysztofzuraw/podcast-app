c678451765e36a24e6ffc2547578d0ac7bd61a6d
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2017-06-26 18:58:29 +0200

    Render basic results

--- src/components/SearchPodcastResults.jsx
+++ src/components/SearchPodcastResults.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
 
 class SearchPodcastResults extends Component {
+  constructor() {
+    super();
+
+    this.renderResults = this.renderResults.bind(this);
+  }
+  renderResults(key) {
+    const podcast = this.props.podcasts[key];
+    return (
+      <div key={key}>
+        <h2>{podcast.title}</h2><p>{podcast.description}</p>
+        <img src={podcast.scaled_logo_url} alt={podcast.title} />
+        <p>{podcast.url}</p>
+      </div>
+    );
+  }
+
   render() {
-    return <h1>Results</h1>;
+    return (
+      <div>{Object.keys(this.props.podcasts).map(this.renderResults)}</div>
+    );
   }
 }
 
