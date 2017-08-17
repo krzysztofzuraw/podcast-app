a57339492205e05db03a1899620b70228ae9baaf
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2017-08-17 18:19:34 +0200

    First round of test fixing: App.jsx

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -3,41 +3,67 @@ import { mount } from "enzyme";
 
 import App from "../components/App";
 
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+};
+
+global.localStorage = new LocalStorageMock;
+
 describe("App Component", () => {
   let component;
 
   const state = {
-    podcasts: {
+    liked: {
       0: {
-        liked: false
+        description: 'Second'
       },
+    },
+    unliked: {
       1: {
-        liked: true
-      }
+        description: 'Whatever'
+      },
     }
   };
 
-  beforeAll(() => {
+  beforeEach(() => {
     component = mount(<App />);
     component.setState(state);    
   });
 
   it("calling addPodcast should add podcast to the state", () => {
     component.instance().addPodcast({ 2: { description: "desc" } });
-    expect(Object.keys(component.state("podcasts")).length).toBe(3);
+    expect(Object.keys(component.state("unliked")).length).toBe(2);
   });
 
   it("calling likePodcast should change the state", () => {
-    component.instance().likePodcast(0);
-    expect(component.state('podcasts')[0]).toEqual({
-      liked: true
+    component.instance().likePodcast(1);
+    expect(component.state('liked')[1]).toEqual({
+      description: "Whatever"
     });
   });
 
   it('calling unlikePodcast should change the state', () => {
-    component.instance().unlikePodcast(1);    
-    expect(component.state('podcasts')[1]).toEqual({
-      liked: false
+    component.instance().unlikePodcast(0);    
+    expect(component.state('unliked')[0]).toEqual({
+      description: "Second"
     })
   })
 });
